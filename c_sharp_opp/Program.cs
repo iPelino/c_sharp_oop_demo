c0ef68f9e56f57555b7483e57c988b42cfbe6f71
Pelin Mutanguha <pelmut2000@gmail.com>
2022-05-30 10:47:35 +0200

    Add employee class

---
+++ c_sharp_opp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!");
+var employee1 = new Employee("James Kabera", "IT")
+{
+    // employee1.Department = "Finance";
+    // employee1.EmployeeName = "James Kabera";
+    EmployeeGender = Employee.Gender.Male
+};
+Console.WriteLine(employee1.GetEmployee() +" "+ employee1.EmployeeGender);
